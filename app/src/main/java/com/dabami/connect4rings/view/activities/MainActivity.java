4f95bacfd989807d17cba74f6814274cc3ab0d05
Daniel Bautista <dabami7@gmail.com>
2015-07-10 18:16:02 +0200

    Adding animations

--- app/src/main/java/com/dabami/connect4rings/view/activities/MainActivity.java
+++ app/src/main/java/com/dabami/connect4rings/view/activities/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import dabami.connect4rings.R;
@@ -12,6 +14,7 @@ import dabami.connect4rings.R;
 
 public class MainActivity extends Activity {
 
+    private Animation animAlpha;
     private Button startButton;
 
     @Override
@@ -19,11 +22,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
         setViews();
+        setAnimations();
         setListeners();
     }
 
     private void setViews() {
         startButton = (Button) findViewById(R.id.startButton);
+    }
+
+    private void setAnimations() {
+        animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
     }
 
     private void setListeners() {
@@ -33,6 +41,7 @@ public class MainActivity extends Activity {
     private OnClickListener startButtonOnClickListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
+            v.startAnimation(animAlpha);
             Intent intent = new Intent(getApplicationContext(), GameActivity.class);
             startActivity(intent);
         }
