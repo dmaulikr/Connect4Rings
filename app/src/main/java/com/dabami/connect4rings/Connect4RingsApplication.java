7ecac61132f4c5011d5208445179c1bc611d694e
Daniel Bautista <daniel.bautista@worldline.com>
2015-08-13 09:30:35 +0200

    Application class

---
+++ app/src/main/java/com/dabami/connect4rings/Connect4RingsApplication.java
@@ -0,0 +1,12 @@
+package com.dabami.connect4rings;
+
+import android.app.Application;
+
+public class Connect4RingsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
